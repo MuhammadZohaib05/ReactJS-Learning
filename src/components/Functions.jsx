e225886ed8d04e7784e0bb9788246ff4375b6cf0
MuhammadZohaib05 <muhammadzohaib786522@gmail.com>
2026-08-31 16:08:58 +0500

    Added Functions and Events

---
+++ src/components/Functions.jsx
@@ -0,0 +1,48 @@
+function MyFun(){
+
+    function getName(yourname){
+        return yourname;
+    }
+    
+    const Click = () => {
+        alert("Button was Clicked")
+    }
+
+    const handleInput = (event) => {
+        console.clear()
+        console.log("Value : ", event.target.value)
+    }
+
+    const HandleMouseOver = () => console.log("Mouse is Over the text!")
+    const HandleDoubleClick = () => console.log("Text Double Clicked!")
+
+    const name1 = "Muhammad"
+    const name2 = "Zohaib"
+
+    return (
+        <>
+            <h1><i>Function</i></h1>
+            <h3>Hello {getName(name1)}</h3>
+            <h3>Bye {getName(name2)}</h3>
+            <p>--------------</p>
+
+            <h1><i>Event</i></h1>
+            <button onClick={Click}>Click Me</button>
+            <br />
+            <button onClick={() => alert("Hello Zohaib from inline function")}>Say Hello</button>
+            <br />
+            <p>--------------</p>
+
+            <h1><i>onChange Event</i></h1>
+            <input type="text" onChange={handleInput} placeholder="Type Something" />
+            <p>--------------</p>
+            
+            <h1><i>MouseOver & DoubleClicked</i></h1>
+            <p onMouseOver={HandleMouseOver} onDoubleClick={HandleDoubleClick}>Lorem ipsum dolor sit amet.</p>
+
+        </>
+    )
+
+}
+
+export default MyFun 
